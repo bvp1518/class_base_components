65de7956ec6124ae9922900171bf51a66c868d57
bvp1518 <bvp18061992@gmail.com>
2024-08-09 14:52:59 +0530

    till-219. Adding a First Class-based Component

---
+++ src/components/User.js
@@ -0,0 +1,20 @@
+
+import { Component } from 'react';
+
+import classes from './User.module.css';
+
+
+// =======================Class based component==================
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+} 
+
+// =======================Function based component==================
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
